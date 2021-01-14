edef2b3ea15dc221cc869bdc197e6d88c5f94d02
Emeka Augustine <cafafanscoders@gmail.com>
2021-01-14 16:32:17 +0800

    Add custom command line for superuser creation

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,13 @@ from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
 from app.settings import UPLOAD_FOLDER
 from app.middlewares.AuthMiddleware import AuthMiddleware
+import click
+from flask.cli import with_appcontext
+
+import uuid
+from werkzeug.security import generate_password_hash
+from email_validator import validate_email, EmailNotValidError
+
 
 
 # Init App
@@ -56,6 +63,78 @@ def error_handler(e):
                 "code": code
             }
         }), code
+
+
+# Create Super User Command
+@app.cli.command()
+@with_appcontext
+@click.option(
+    '--first_name', 
+    required=True, 
+    prompt=True, type=str, 
+    help="The First Name of the user"
+)
+
+@click.option(
+    '--last_name', 
+    required=True, 
+    prompt=True, 
+    type=str,
+    help="The Last Name of the user"
+)
+@click.option(
+    '--username', 
+    required=True, 
+    prompt=True, 
+    type=str,
+    help="The Username of the user"
+)
+@click.option(
+    '--email', 
+    required=True, 
+    prompt=True, 
+    type=str,
+    help="The Email of the user"
+)
+@click.option(
+    '--password', 
+    prompt=True, 
+    required=True, 
+    hide_input=True, 
+    confirmation_prompt=True,
+    help="The Password of the user"
+)
+def create_super_user(*args, **kwargs):
+    """Create Super User."""
+    from app.users.models.User import User
+    from app.profiles.models.Profile import Profile
+
+    user = User()
+    profile = Profile()
+
+    user.first_name = kwargs.get('first_name')
+    user.last_name  = kwargs.get('last_name')
+    user.username   = kwargs.get('username')
+    user.email  = kwargs.get('email')     
+    # Hash The Password
+    user.password = generate_password_hash(
+        kwargs.get('password'), 
+        method='sha256'
+    )
+    user.user_public_id   = str(uuid.uuid4())
+    user.is_admin   = True
+
+    # Create Profile Here....
+    profile.name  = "{} {}".format(user.first_name, user.last_name)
+    profile.profile_public_id =  str(uuid.uuid4())
+    profile.user  = user
+
+    # # Add the user to the database
+    db.session.add(user)
+    # Then commit the session
+    db.session.commit()
+
+    click.echo('Super User Created Successfully!')
 
 
 def create_app():
